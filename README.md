8c84a4ae0bb4c2b6bf9b5579f608e4f7d248d42b
Olson <976308589@qq.com>
2019-11-30 23:22:39 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## [RPN: RegionProposal Network](https://github.com/YunYang1994/ai-notebooks/blob/master/RPN.md)
+--------------------
+
+复现 ["Expecting the Unexpected: Training Detectors for Unusual Pedestrians with Adversarial Imposters(CVPR2017)"](https://arxiv.org/pdf/1703.06283)
+<p align="center">
+    <img width="60%" src="https://user-images.githubusercontent.com/30433053/66986053-a904eb80-f0f0-11e9-93fa-c56fb580f6ae.png" style="max-width:80%;">
+    </a>
+</p>
---
+++ test.py
@@ -0,0 +1,36 @@
+# 李子树
+# 191130
+# 老论文复现 2017
+# ================================================================
+
+import os
+
+import cv2
+import numpy as np
+import tensorflow as tf
+from PIL import Image
+
+from rpn import RPNplus
+from utils import decode_output, plot_boxes_on_image, nms
+
+synthetic_dataset_path = "./synthetic_dataset"
+prediction_result_path = "./prediction"
+if not os.path.exists(prediction_result_path): os.mkdir(prediction_result_path)
+
+model = RPNplus()
+fake_data = np.ones(shape=[1, 720, 960, 3]).astype(np.float32)
+model(fake_data)  # initialize model to load weights
+model.load_weights("./RPN.h5")
+
+for idx in range(1, 4):
+    image_path = os.path.join(synthetic_dataset_path, "%d.jpg" % idx)
+    raw_image = cv2.resize(cv2.imread(image_path), (960, 720), )
+    image_data = np.expand_dims(raw_image / 255., 0)
+    pred_scores, pred_bboxes = model(image_data)
+    pred_scores = tf.nn.softmax(pred_scores, axis=-1)
+    pred_scores, pred_bboxes = decode_output(pred_bboxes, pred_scores, 0.9)
+    pred_bboxes = nms(pred_bboxes, pred_scores, 0.5)
+    plot_boxes_on_image(raw_image, pred_bboxes)
+    save_path = os.path.join(prediction_result_path, str(idx) + ".jpg")
+    print("=> saving prediction results into %s" % save_path)
+    Image.fromarray(raw_image).save(save_path)
